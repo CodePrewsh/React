a7681d31fa2c3cfd217aff9ebb5f6ce9f0676d24
precious <bhembepreciouspt@gmail.com>
2024-05-26 01:48:19 +0200

    Tutorials

---
+++ Tutorial5/react-course-final-master/small-shopping-cart/src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Collen Hover Books',
+      price: 1400,
+      image: 'https://www.beyondthebookends.com/wp-content/uploads/2024/02/Colleen-Hoover-Books.jpg',
+    },
+    {
+      id: '2',
+      name: 'Hyudai i30N',
+      price:  74900,
+      image: 'https://www.hyundai-n.com/images/en/model/n-i30N-section02-02.jpg',
+    },
+    {
+      id: '3',
+      name: 'Vans off the wall',
+      price: 1000,
+      image: 'https://cdn.shopify.com/s/files/1/0802/5836/7772/files/VN000D1J689_ALT1.jpg?width=500',
+    },
+  ],
+};
+export default data;
