64e91696deb06a35f316f5598d1c1edd3a34bee9
precious <bhembepreciouspt@gmail.com>
2024-07-03 08:50:37 +0200

    updates

--- Tutorials/INSTA CLONE/src/App.jsx
+++ Tutorials/INSTA CLONE/src/App.jsx
@@ -1,11 +1,23 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import PageLayout from "./Layouts/PageLayout/PageLayout";
+// import ProfilePage from "./pages/ProfilePage/ProfilePage";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "./firebase/firebase";
 
 function App() {
-  return (
-    <> 
-      <Button colorScheme="blue">Hello</Button>
-    </>
-  );
+	const [authUser] = useAuthState(auth);
+
+	return (
+		<PageLayout>
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/auth' />} />
+				<Route path='/auth' element={!authUser ? <AuthPage /> : <Navigate to='/' />} />
+			
+			</Routes>
+		</PageLayout>
+	);
 }
 
 export default App;
