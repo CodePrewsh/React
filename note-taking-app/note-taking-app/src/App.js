e4fa874034148a9d8435c9f12a67bc2ee323ad4b
precious <bhembepreciouspt@gmail.com>
2024-07-10 11:52:07 +0200

    Project

--- note-taking-app/note-taking-app/src/App.js
+++ note-taking-app/note-taking-app/src/App.js
@@ -1,16 +1,16 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NoteList from './components/noteList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NoteList from './components/NoteList';
 import './App.css';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" component={NoteList} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<NoteList />} />
+      </Routes>
     </Router>
   );
 };
